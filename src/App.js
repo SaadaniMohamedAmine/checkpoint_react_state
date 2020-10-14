370f8f12240decc6f8d4c4c8047a69fb58f4f750
Saadani Mohmaed Amine <saadanimohamedamine@gmail.com>
2020-10-14 18:21:20 +0200

    Add timer to component (start from copont mounted)

--- src/App.js
+++ src/App.js
@@ -12,16 +12,39 @@ class App extends React.Component {
       imgSrc:imgSrc ,
     },
     show:false,
+    time:0,
   };
    toggleShow=()=>{
     this.setState({show:!(this.state.show)}) ;
-   } 
+   }
+    count=()=>{
+    
+    }
+    componentDidMount() {
+        setInterval(() => {
+            this.state.show &&
+          this.setState({time:this.state.time+1})
+          
+         }, 1000)
+      
+    
+    }
+   
+   /*componentDidMount () {
+    ( setInterval(() => {
+      this.setState({time:this.state.time+1})
+      
+     }, 1000))*/
+    
+
+   
    
   
   render()
   {
     return (
       <div>
+        
         <button onClick={this.toggleShow} className="btn">{this.state.show? "Hide":"Show"}</button>
         {this.state.show &&   
         <div id="component">
@@ -29,7 +52,8 @@ class App extends React.Component {
           <div className="item"><span className="key">Profession : </span>{this.state.Person.profession}</div>
           <div className="item"><span className="key">Biographie: </span><span id="bio">{this.state.Person.bio}</span></div>
           <div className="item"><img src={this.state.Person.imgSrc } id="img" /></div>
-        </div>
+        <p style={{color:"#fff"}}>{this.state.time}</p>
+        </div> 
         }
       </div>
       
